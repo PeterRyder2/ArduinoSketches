4a35da6040c1d671305c43087d645f860eb77318
Peter Ryder <peter.ryder2@gmail.com>
2019-07-07 22:23:52 +0100

    addded if statment

--- test.ino
+++ test.ino
@@ -35,28 +35,46 @@ void setup()
 void loop()
 {
 
-	  dht1.readHumidity();
-	  dht1.readTemperature();
-	  // Check if any reads failed and exit early (to try again).
-	  if (isnan(dht1.humidity) || isnan(dht1.temperature_C)) {
-	    Serial.println("DHT sensor read failure!");
-	    return;
-	  }
+	dht1.readHumidity();
+	dht1.readTemperature();
+	// Check if any reads failed and exit early (to try again).
+	if (isnan(dht1.humidity) || isnan(dht1.temperature_C)) {
+	Serial.println("DHT sensor read failure!");
+	return;
+	}
 
-	  Serial.print(dht1.humidity); Serial.print(" %\t\t");
-	  Serial.print(dht1.temperature_C); Serial.println(" *C\t");
-	  // Wait a few seconds between measurements. The DHT22 should not be read at a higher frequency of
-	  // about once every 2 seconds. So we add a 3 second delay to cover this.
-	  delay(3000);
+	Serial.print(dht1.humidity); Serial.print(" %\t\t");
+	Serial.print(dht1.temperature_C); Serial.println(" *C\t");
+	// Wait a few seconds between measurements. The DHT22 should not be read at a higher frequency of
+	// about once every 2 seconds. So we add a 3 second delay to cover this.
+	delay(3000);
 
-	  //mSensor.SetReading();
-	  //Serial.print("Moisture is: ");
-	  //Serial.println(mSensor.GetReading());
+	//mSensor.SetReading();
+	//Serial.print("Moisture is: ");
+	//Serial.println(mSensor.GetReading());
 
-	  ls1.SetReading();
-	  Serial.print("LightSensor is: ");
-	  Serial.println(ls1.GetReading());
-	  relay1.On();
-	  delay(110);
-	  relay1.Off();
+	ls1.SetReading();
+	Serial.print("LightSensor is: ");
+	Serial.println(ls1.GetReading());
+	mSensor.SetReading();
+	
+	Serial.print("MoistureSensor READING  is: ");
+	temp = mSensor.GetReading();
+	Serial.println(temp);
+
+	if (temp > 0 ){
+		relay1.On();
+		delay(110);
+		relay1.Off();
+	}
+	
+/*
+	relay1.On();
+	delay(110);
+	relay1.Off();
+ */
+
+
+	
+
 }
